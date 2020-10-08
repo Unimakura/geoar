e6acd2f6d3e04e873c5f5c013b9b8389b6984d3d
UNI-PC2020\Uni <mayarune@yahoo.co.jp>
2020-10-08 11:52:53 +0900

    ios13+対応

--- script.js
+++ script.js
@@ -114,7 +114,26 @@ window.onload = () => {
 		addData(title,lat,lng);
         }
         }
-	};
+    };
+    
+    // iOS13+ 用にパーミッションを取得する
+    if (
+        DeviceMotionEvent &&
+        DeviceMotionEvent.requestPermission &&
+        typeof DeviceMotionEvent.requestPermission === 'function'
+        )
+    {
+        DeviceMotionEvent.requestPermission();
+    }
+
+    if (
+        DeviceOrientationEvent &&
+        DeviceOrientationEvent.requestPermission &&
+        typeof DeviceOrientationEvent.requestPermission === 'function'
+        )
+    {
+        DeviceOrientationEvent.requestPermission();
+    }
 
 	readData();
 
